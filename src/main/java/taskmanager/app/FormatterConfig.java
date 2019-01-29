f6836ab6bbe0d47e1874c36bbb941973b8182902
miroslawgrzadziel <mgrzadziel@interia.pl>
2019-01-29 10:28:54 +0100

    Creating project structure

---
+++ src/main/java/taskmanager/app/FormatterConfig.java
@@ -0,0 +1,21 @@
+package taskmanager.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "taskmanager")
+@EnableTransactionManagement
+public class FormatterConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+    }
+}
